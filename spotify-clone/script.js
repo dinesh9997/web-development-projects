60bc3f7d3ba537e23174993faa739f3920a8402d
dinesh9997 <dineshgujju9997@gmail.com>
2026-01-30 21:13:05 +0530

    added playlist of musics using js and updated css files

--- spotify-clone/script.js
+++ spotify-clone/script.js
@@ -17,25 +17,37 @@ async function getSongs(){
 
 
 
-return songs
+ return songs
 }
 async function main(){
 
 
     let songs=await getSongs()
     console.log(songs)
-    var x=9;
+    // var x=9;
 
-    var audio=new Audio(songs[x])
-    audio.play()
-    audio.addEventListener("loadeddata", () => {
-        let dur=audio.duration;
-        console.log(dur)
-    })
+    // var audio=new Audio(songs[x])
+    // audio.play()
+    // audio.addEventListener("loadeddata", () => {
+    //     let dur=audio.duration;
+    //     console.log(dur)
+    // })
 
     var songul=document.querySelector(".listsongs").getElementsByTagName("ul")[0];            
     for (const song of songs) {
-        songul.innerHTML=songul.innerHTML+`<li>${song.replaceAll("%20","")}</li>`;
+        songul.innerHTML=songul.innerHTML+`<li>
+        <img class="invert" src="/spotify-clone/icons/music.svg" alt="musicicon">
+                <div class="songinfo">
+                    <div>${song.replaceAll("%20","")}</div>
+                    <div>~Dinesh</div>
+                </div>
+                <div class="playmusic" >
+                    <span>Play Now</span>
+                    <img class="invert" src="/spotify-clone/icons/playicon.svg" alt=""></div>
+        
+        
+        
+        </li>`;
     }
 
 }
