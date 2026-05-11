f0d470bf9f6a3dadbf276ecba4051407a298502e
dinesh9997 <dineshgujju9997@gmail.com>
2026-05-11 12:21:04 +0530

    fixed server paths

--- spotify-clone/server.js
+++ spotify-clone/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const serveIndex = require("serve-index");
 const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "spotify-clone")));
+
+app.use(
+  "/songs",
+  serveIndex(path.join(__dirname, "spotify-clone/songs"), {
+    icons: true,
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server Running");
+  console.log(`Server running on port ${PORT}`);
 });
