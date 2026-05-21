4630b8d62aeb76c99061cd82d063ffa60c2c682e
dinesh9997 <dineshgujju9997@gmail.com>
2026-05-21 22:36:13 +0530

    added some new songs

--- spotify-clone/server.js
+++ spotify-clone/server.js
@@ -5,13 +5,13 @@ const serveIndex = require("serve-index");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-// Serve static files
+
 app.use(express.static(__dirname));
 
-// Enable folder listing for songs
+
 app.use("/songs", serveIndex(path.join(__dirname, "songs"), { icons: true }));
 
-// Open index.html on home route
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
