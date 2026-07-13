230469ec4b0a6487b7503a18293137da50ba9c58
dinesh9997 <dineshgujju9997@gmail.com>
2026-07-13 21:10:18 +0530

    added delete and edit feature

--- TodolistApp/src/App.jsx
+++ TodolistApp/src/App.jsx
@@ -1,29 +1,51 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar'
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
- const [todo,setTodo]=useState("") // for input
-  const [todos,setTodos]=useState([])                      // holds all todos in array
+  const [todo, setTodo] = useState("") // for input
+  const [todos, setTodos] = useState([])                      // holds all todos in array
 
 
-  const handleAdd=()=>{
-          setTodos([...todos,{todo,isCompleted:false}])
-          setTodo("")
-          console.log(todos)
-          
-        }
+  const handleAdd = () => {
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
+    setTodo("")
+    console.log(todos)
+
+  }
+  const handleCheckbox = (e) => {
+    let id = e.target.name;
+
+    let index = todos.findIndex(item => {
+      return item.id === id;
+    })
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos)
+  }
 
 
+  const handleDelete = (e, id) => {
+    console.log(`The id is ${id}`)
+  }
 
-  const handleDelete=()=>{
+  const handleEdit = () => {
+    let index = todos.findIndex(item => {
+      return item.id === id;
+    })
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos)
+
 
   }
 
-  const handleEdit=()=>{
 
-  }
-  const handleChange=(e)=>{
-setTodo(e.target.value)
+
+
+  const handleChange = (e) => {
+    setTodo(e.target.value)
   }
   return (
     <>
@@ -36,20 +58,23 @@ setTodo(e.target.value)
         </div>
         <h2 className='text-lg font-bold'>Your Todos</h2>
         <div className="todos">
-          {todos.map((item,index)=>{
+          {todos.map((item) => {
+            /*here serach "uuid npm" where this pakage is used for asigning unique ids to eah todo so that fo that todo by their id w an perfor our taks
+            terminal run: npm run uuid
+            add at top: import { v4 as uuidv4 } from 'uuid';
+            */
 
-          
-         return <div key={index} className="todo flex w-1/4 justify-between">
-          <input type="checkbox" value={todo.isCompleted} name="" id="" />
-            <div className={item.isCompleted?"line-through":""} >{item.todo}</div>
-            <div className="buttons ">
-              <button onClick={handleEdit} className='cursor-pointer bg-violet-700 hover:bg-violet-900  text-sm hover:font-bold p-2 py-1 text-white rounded-md mx-1'>Edit </button>
-              <button onClick={handleDelete} className='cursor-pointer bg-violet-700 hover:bg-violet-900  text-sm hover:font-bold p-2 py-1 text-white rounded-md mx-1' >Delete</button>
+            return <div key={item.id} className="todo flex w-1/4 justify-between">
+              <input name={item.id} onChange={handleCheckbox} type="checkbox" value={item.isCompleted} id="" />
+              <div className={item.isCompleted ? "line-through" : ""} >{item.todo}</div>
+              <div className="buttons ">
+                <button onClick={handleEdit} className='cursor-pointer bg-violet-700 hover:bg-violet-900  text-sm hover:font-bold p-2 py-1 text-white rounded-md mx-1'>Edit </button>
+                <button onClick={(e) => { handleDelete(item.id) }} className='cursor-pointer bg-violet-700 hover:bg-violet-900  text-sm hover:font-bold p-2 py-1 text-white rounded-md mx-1' >Delete</button>
+
+              </div>
+
 
             </div>
-
-
-          </div>
           })}
         </div>
       </div>
