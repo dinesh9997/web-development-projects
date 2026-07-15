90340a61879b192a98005472851385afa8a9281c
dinesh9997 <dineshgujju9997@gmail.com>
2026-07-15 22:23:10 +0530

    implemented localstorage

--- TodolistApp/src/App.jsx
+++ TodolistApp/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import NavBar from './components/NavBar'
 import { v4 as uuidv4 } from 'uuid';
+
 
 
 function App() {
   const [todo, setTodo] = useState("") // for input
   const [todos, setTodos] = useState([])                      // holds all todos in array
+ useEffect(()=>{
+  let todoString= localStorage.getItem("todos")
+  if(todoString){
+    let todos=JSON.parse(localStorage.getItem("todos"))
+    setTodos(todos)
 
+  }
+ },[])
+
+  const saveToLS=()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  }
 
   const handleAdd = () => {
     setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
     setTodo("")
     console.log(todos)
+    saveToLS()
 
   }
   const handleCheckbox = (e) => {
@@ -23,6 +36,8 @@ function App() {
     let newTodos = [...todos];
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos)
+    saveToLS()
+
   }
 
 
@@ -32,12 +47,14 @@ function App() {
       return item.id!==id
     });
     setTodos(newTodos)
+    saveToLS()
 
   }
 
   const handleEdit = (e,id) => {
-    
-
+   let t= todos.filter(i=>i.id===id)
+setTodo(t[0].todo)
+    saveToLS()
 
   }
 
@@ -46,6 +63,8 @@ function App() {
 
   const handleChange = (e) => {
     setTodo(e.target.value)
+
+
   }
   return (
     <>
@@ -68,7 +87,7 @@ function App() {
             return <div key={item.id} className="todo flex w-1/4 justify-between">
               <input name={item.id} onChange={handleCheckbox} type="checkbox" value={item.isCompleted} id="" />
               <div className={item.isCompleted ? "line-through" : ""} >{item.todo}</div>
-              <div className="buttons ">
+              <div className="buttons flex">
                 <button onClick={(e)=> handleEdit(e,item.id)} className='cursor-pointer bg-violet-700 hover:bg-violet-900  text-sm hover:font-bold p-2 py-1 text-white rounded-md mx-1'>Edit </button>
                 <button onClick={(e) => { handleDelete(e,item.id) }} className='cursor-pointer bg-violet-700 hover:bg-violet-900  text-sm hover:font-bold p-2 py-1 text-white rounded-md mx-1' >Delete</button>
 
